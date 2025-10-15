f3bbd0e4907fb22e8b7b0eabfd9f41f1502354f3
vegas <abdelahe53@gmail.com>
2025-10-15 18:54:41 +0100

    a

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,19 +1,29 @@
 #include <unistd.h>
 
-int	main()
+int main()
 {
-	int j = '0';
-	int a = '1';
-	int z = '0';
+	int a = '0';
+	int b = '1';
+	int c = '2';
 
-while(j <= '9')
+	while(a <= '7')
 	{
-	write(1, &j, 1);
-	while(z < '9')
-	{
-		write(1, &a, 1);
-		write(1, &z, 1);
-	z++;
+		while(b <= '8')
+		{
+			while(c <= '9')
+			{
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
+				if(a < '7' || b < '8' || c < '9')
+				write(1, ",", 1);
+			c++;
+			}
+			c = b + 2; //2nd loop
+		b++;
+		}
+		b = a + 1;
+	a++;
 	}
-j++;}
+write(1, "\n", 1);
 }
