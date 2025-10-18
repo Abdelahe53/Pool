3f2b4680dec9e62723bbe933e1a75367227730f1
vegas <abdelahe53@gmail.com>
2025-10-18 16:57:43 +0100

    a

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,24 +1,30 @@
 #include <unistd.h>
 
-int main()
+void	ft_print_comb(void)
 {
+	int	a;
 	int	b;
 	int	c;
 
-	b = '0';
-	c = '1';
-	while (b <= '8')
+	a = '0';
+	while (a <= '7')
 	{
-		while (c <= '9')
+		b = a + 1;
+		while (b <= '8')
 		{
-			write(1, &b, 1);
-			write(1, &c, 1);
-			if (b < '8' || c < '9')
-				write(1, ",", 1);
-			c++;
+			c = b + 1;
+			while (c <= '9')
+			{
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
+				if (a < '7' || b < '8' || c < '9')
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
 		}
-		c = b + 1;
-		b++;
+		a++;
 	}
 	write(1, "\n", 1);
 }
