9e9ebbcc19439cbd25f2f4d112e8a13793757e68
vegas <abdelahe53@gmail.com>
2025-10-26 13:51:26 +0100

    a

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,12 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("%d \n", INT_MAX);
+	printf("%d \n", INT_MIN);
+}
+
+ - Write() prints only one character,
+ - To print multiple numbers using write you need a loop,
+	. Let the user be able to print the whole mximum number first
