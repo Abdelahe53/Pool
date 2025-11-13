f7f570697685f0bce9fee2d30329686343558410
vegas <abdelahe53@gmail.com>
2025-11-13 15:45:56 +0100

    x

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -4,16 +4,31 @@ void ft_print_comb2(void)
 {
     int a;
     int b;
+    int c;
+    int d;
 
     a = '0';
     while (a <= '9')
     {
-        b = a + 1;
+        b = a;
         while (b <= '9')
         {
-            write(1, &a, 1);
-            write(1, &b, 1);
-            write(1, " ", 1);
+            c = b;
+            while (c <= '9')
+            {
+                d = c + 1;
+                while (d <= '9')
+                {
+                    write(1, &a, 1);
+                    write(1, &b, 1);
+                    write(1, " ", 1);
+                    write(1, &c, 1);
+                    write(1, &d, 1);
+                    write(1, ", ", 2);
+                    d++;
+                }
+                c++;
+            }
             b++;
         }
         a++;
