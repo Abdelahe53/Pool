ac1605b463f007ed621f91fb2ce02a6ac84ed14c
Abdellah git <abdelahe53@gmail.com>
2026-02-09 15:35:46 +0100

    a

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -8,23 +8,16 @@ void ft_putnbr(int nb)
 {
     int rest;
 
-    if (nb == 2147483647)
-        write(1, "2147483647", 10);
-
-    else if (nb == -2147483648)
+    if (nb == -2147483648)
     {
         write(1, "-", 1);
         write(1, "2147483648", 10);
     }
-    else if (nb > 9)
+    else if (nb > 0)
     {
-        nb = nb % 10 + '0';
-        write(1, &nb, 1);
-        ft_putnbr(nb);
-    }
-    else
-    {
-        write(1, &nb, 1);
+        rest = nb % 10 + '0';
+	ft_putnbr(nb / 10);
+        write(1, &rest, 1);
     }
 }
 
