03e89e237cc632abf87425248f4141d29fd4308d
Mr.Vegas <74936090+Abdelahe53@users.noreply.github.com>
2026-02-19 21:56:22 +0000

    Fix ft_putnbr to handle INT_MIN and update main

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -2,7 +2,6 @@
 //INT_MIN: 2147483648
 
 #include <unistd.h>
-#include <stdio.h>
 
 void ft_putnbr(int nb)
 {
@@ -10,18 +9,22 @@ void ft_putnbr(int nb)
 
     if (nb == -2147483648)
     {
-        write(1, "-", 1);
-        write(1, "2147483648", 10);
+        write(1, "-2147483648", 11);
     }
-    else if (nb > 0)
+    if (nb / 10 > 0)
     {
+        ft_putnbr(nb / 10);
         rest = nb % 10 + '0';
-	ft_putnbr(nb / 10);
         write(1, &rest, 1);
+    }
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        ft_putnbr(nb * -1);
     }
 }
 
 int main()
 {
-    ft_putnbr(243);
+    ft_putnbr(1223);
 }
